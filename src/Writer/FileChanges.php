15e37dbbc5d5128f5abab153f2921dc0c3818392
Christian Deinert <ChDeinert@gmail.com>
2017-01-18 23:46:06 +0100

    Add writer for change summary

---
+++ src/Writer/FileChanges.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Copyright (c) Andreas Heigl<andreas@heigl.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright 2016-2016 Andreas Heigl
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @since     16.06.2016
+ * @link      http://github.com/heiglandreas/org.heigl.junitdiff
+ */
+
+namespace Org_Heigl\JUnitDiff\Writer;
+
+use Org_Heigl\JUnitDiff\MergeResult;
+use Symfony\Component\Console\Style\StyleInterface;
+
+class FileChanges implements WriterInterface
+{
+    protected $style;
+
+    protected $file1;
+
+    protected $file2;
+
+    public function __construct(StyleInterface $style, $file1, $file2)
+    {
+        $this->style = $style;
+        $this->file1 = $file1;
+        $this->file2 = $file2;
+    }
+
+    public function write(MergeResult $mergeResult)
+    {
+        $this->style->text(sprintf(
+            '<fg=yellow>Added: %s, Removed: %s, Changed: %s</>',
+            $mergeResult->countNew(),
+            $mergeResult->countRemoved(),
+            $mergeResult->countChanged()
+        ));
+    }
+}
---
+++ tests/Writer/FileChangesTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Copyright (c) Andreas Heigl<andreas@heigl.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright 2016-2016 Andreas Heigl
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @since     16.06.2016
+ * @link      http://github.com/heiglandreas/org.heigl.junitdiff
+ */
+
+namespace Org_Heigl\JUnitDiffTest\Writer;
+
+use Mockery as M;
+use Org_Heigl\JUnitDiff\Writer\FileChanges;
+
+class FileChangesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testThatChangesSummaryWorks()
+    {
+        $styleInterface = M::mock('\Symfony\Component\Console\Style\StyleInterface');
+        $styleInterface->shouldReceive('text')
+            ->with('<fg=yellow>Added: 3, Removed: 5, Changed: 7</>');
+
+        $mergeresult = M::mock('\Org_Heigl\JUnitDiff\MergeResult');
+        $mergeresult->shouldReceive('countNew')->andReturn(3);
+        $mergeresult->shouldReceive('countRemoved')->andReturn(5);
+        $mergeresult->shouldReceive('countChanged')->andReturn(7);
+
+        $fileChanges = new FileChanges($styleInterface, 'a', 'b');
+        $this->assertNull($fileChanges->write($mergeresult));
+    }
+}
